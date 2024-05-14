89340d7ca38c0b4175409a64c4cf7239924e09d3
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-05-14 22:11:51 +0530

    refactor: Update routes and controllers for admin-related functionality

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,39 @@
 <?php
 
-use App\Http\Controllers\DrugController;
+use App\Http\Controllers\Admin\AddQuotationController;
+use App\Http\Controllers\Admin\DrugController as AdminDrugController;
+use App\Http\Controllers\Admin\PrescriptionController as AdminPrescriptionController;
+use App\Http\Controllers\Admin\QuotationController as AdminQuotationController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuotationController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('/prescription', PrescriptionController::class)->middleware(['auth', 'verified']);
+Route::resource('/quotation', QuotationController::class)->middleware(['auth', 'verified']);
+
 
 Route::middleware(['auth', 'role:admin'])->name('admin/')->prefix('admin')->group(function () {
     Route::get('/dashboard', function () {
         return view('admin.dashboard');
     })->name('dashboard');
-
-    Route::resource('users', UserController::class);
-    Route::resource('drugs', DrugController::class);
-    Route::resource('quotation', QuotationController::class);
+    Route::resource('users', AdminUserController::class);
+    Route::resource('drugs', AdminDrugController::class);
+    Route::resource('prescriptions', AdminPrescriptionController::class);
+    Route::resource('quotations', AdminQuotationController::class);
+    Route::resource('quotation-details', AddQuotationController::class);
 });
 
 Route::middleware('auth')->group(function () {
